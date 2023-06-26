1e6e9a4929c401ae8710b69fdee896965dc398af
ezzo <yalazzi88@gmail.com>
2023-06-27 00:03:36 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+const nav = document.querySelector("nav");
+// const btn = document.querySelector("button");
+const ul = document.querySelector("ul");
+const main = document.getElementById("main");
+const header = document.getElementById("header");
+const mode = document.getElementById("btn-container");
+
+
+
+// btn.addEventListener("click",(eo)=>{
+  
+    
+  
+    
+
+// });
+
+
+
+
+  mode.addEventListener("change",function(eo){  
+    main.classList.toggle("dark");
+    
+  
+    
+  
+    
+    
+
+});
+
+
+
+
+
+
+
+
+
+
